da3ded68fbbb5c87d5d6c4a880ef0994bff8974a
angel00268 <ss.2015015@gmail.com>
2022-03-26 14:15:24 -0600

    create: Migrations - Controladores - Seeders |  update: User

---
+++ app/Models/InvestigationTag.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class InvestigationTag extends Model
+{
+    use HasFactory;
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Http\Controllers\Tag;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,5 +16,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call([
+            DepartmentSeeder::class,
+            UserSeeder::class,
+            StatusSeeder::class,
+            InvestigationSeeder::class,
+            TagSeeder::class,
+            InvestigationTagSeeder::class,
+        ]);
     }
 }
